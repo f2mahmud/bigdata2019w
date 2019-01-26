e41f826b31c646643e4d4303eb752b8b4a53b140
Fahim Mahmud <f2mahmud@uwaterloo.ca>
2019-01-26 12:45:03 -0500

    WIP::a3

--- src/main/scala/ca/uwaterloo/cs451/a3/BigramRelativeFrequencyStripes.scala
+++ src/main/scala/ca/uwaterloo/cs451/a3/BigramRelativeFrequencyStripes.scala
@@ -9,14 +9,15 @@ import org.apache.spark.SparkConf
 import org.rogach.scallop._
 
 class Conf(args: Seq[String]) extends ScallopConf(args) {
+
   mainOptions = Seq(input, output, reducers)
-  val input = opt[String](descr = "input path", required = true)
-  val output = opt[String](descr = "output path", required = true)
+  val input: ScallopOption[String] = opt[String](descr = "input path", required = true)
+  val output: ScallopOption[String] = opt[String](descr = "output path", required = true)
   val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
   verify()
 }
 
-object BigramCount extends Tokenizer {
+object BigramRelativeFrequencyStripes extends Tokenizer {
   val log = Logger.getLogger(getClass().getName())
 
   def main(argv: Array[String]) {
