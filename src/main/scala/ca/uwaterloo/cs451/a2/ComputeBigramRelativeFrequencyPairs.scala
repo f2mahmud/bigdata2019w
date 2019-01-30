2464a08361cacdff10843f260c0f245f9c91b6a4
Fahim Mahmud <f2mahmud@uwaterloo.ca>
2019-01-29 21:03:26 -0500

    WIP:a2

--- src/main/scala/ca/uwaterloo/cs451/a2/ComputeBigramRelativeFrequencyPairs.scala
+++ src/main/scala/ca/uwaterloo/cs451/a2/ComputeBigramRelativeFrequencyPairs.scala
@@ -6,14 +6,19 @@ import org.apache.hadoop.fs._
 import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 import org.rogach.scallop._
 
-import scala.collection.mutable
 
 class ConfPairsBRF(args: Seq[String]) extends ScallopConf(args) {
   mainOptions = Seq(input, output, reducers)
-  val input: ScallopOption[String] = opt[String](descr = "input path", required = true)
-  val output: ScallopOption[String] = opt[String](descr = "output path", required = true)
-  val reducers: ScallopOption[Int] = opt[Int](descr = "number of reducers", required = false, default = Some(1))
-  var threshold: ScallopOption[Int] = opt[Int](descr = "threshold for total count", required = false, default = Some(0))
+  val input: ScallopOption[String] =
+    opt[String](descr = "input path", required = true)
+  val output: ScallopOption[String] =
+    opt[String](descr = "output path", required = true)
+  val reducers: ScallopOption[Int] =
+    opt[Int](descr = "number of reducers", required = false, default = Some(1))
+  var threshold: ScallopOption[Int] = opt[Int](descr =
+    "threshold for total count",
+    required = false,
+    default = Some(0))
   verify()
 }
 
@@ -26,11 +31,10 @@ object ComputeBigramRelativeFrequencyPairs extends Tokenizer {
     log.info("Input: " + args.input())
     log.info("Output: " + args.output())
     log.info("Number of reducers: " + args.reducers())
-    log.info("Number of reducers: " + args.threshold())
 
     val conf = new SparkConf().setAppName("Bigram Count")
+    conf.set("spark.default.parallelism", args.reducers().toString)
     val sc = new SparkContext(conf)
-    sc.getConf.set("spark.default.parallelism",args.reducers().toString)
 
     val outputDir = new Path(args.output())
     FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
@@ -47,14 +51,17 @@ object ComputeBigramRelativeFrequencyPairs extends Tokenizer {
           else tokens.map(token => List(token, "*"))
         }
       )
-      .map{case(List(x,y)) => (x,y)}
-      .map(bigram => (bigram,1f))
+      .map { case (List(x, y)) => (x, y) }
+      .map(bigram => (bigram, 1f))
       .sortByKey()
-      .reduceByKey(_ + _)
+      .reduceByKey(_ + _, args.reducers())
+      .sortByKey()
       .map({
         case (key, value) => {
-          if (key._2 == "*")
+          if (key._2 == "*") {
             marginal = value
+            (key, value)
+          }
           else {
             (key, value / marginal)
           }
