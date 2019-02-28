08711bab3ef47ea90b1be6e46fecbc664e5a4425
Fahim Mahmud <f2mahmud@uwaterloo.ca>
2019-02-28 12:46:58 -0500

    First mass done

--- src/main/java/ca/uwaterloo/cs451/a4/ExtractTopPersonalizedPageRankNodes.java
+++ src/main/java/ca/uwaterloo/cs451/a4/ExtractTopPersonalizedPageRankNodes.java
@@ -64,7 +64,6 @@ public class ExtractTopPersonalizedPageRankNodes extends Configured implements T
         @Override
         public void map(IntWritable nid, PersonalizedPageRankNode node, Context context) throws IOException,
                 InterruptedException {
-            System.out.println(">>>>>>>>>>>>>>>>>node id  " + node);
             for (int i = 0; i < sources.size(); i++) {
                 queues.get(i).add(node.getNodeId(), node.getPageRank(i));
             }
