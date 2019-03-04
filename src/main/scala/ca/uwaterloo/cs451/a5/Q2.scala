9c9282f97fc6164b05a0f786b2abd9b6cfaf9dfe
Fahim Mahmud <f2mahmud@uwaterloo.ca>
2019-03-04 12:50:39 -0500

    q2 text and parquet

--- src/main/scala/ca/uwaterloo/cs451/a5/Q2.scala
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q2.scala
@@ -107,8 +107,8 @@ object Q2 {
     orders.registerTempTable("orders")
     println("Given >>>>>>>>>> ")
 
-    val initialSql = sqlContext.sql("select * from orders").show()
-    val initialSql2 = sqlContext.sql("select * from lineitem").show()
+    val initialSql = sqlContext.sql("select o_clerk, o_orderkey from orders").show()
+    val initialSql2 = sqlContext.sql("select * from lineitem where l_shipdate = " + date).show()
     val sqlAns = sqlContext.sql("select o_clerk, o_orderkey from lineitem, orders where l_orderkey = o_orderkey and l_shipdate = " +
       date + " order by o_orderkey asc limit 20").show()
 
