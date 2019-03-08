5e7ec20c0431608cc509ad3bc2be9e5f3c6e5e11
Fahim Mahmud <f2mahmud@uwaterloo.ca>
2019-03-08 17:26:37 -0500

    Q4 text

--- src/main/scala/ca/uwaterloo/cs451/a5/Q4.scala
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q4.scala
@@ -73,7 +73,7 @@ object Q4 {
         .flatMap(_._2._2)
         .map(customers.value(_))
         .reduceByKey(_ + _)
-        .sortBy(_._1, true)
+        .sortBy(_._1, true, numPartitions = 1)
         .foreach(item => {
           println("(" + item._1._1 + "," + item._1._2 + "," + item._2 + ")")
         })
