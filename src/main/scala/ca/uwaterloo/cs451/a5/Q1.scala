59aa9e730a14cc87e8144e0841c64c370cd2bed8
Fahim Mahmud <f2mahmud@uwaterloo.ca>
2019-03-10 20:27:29 -0400

    Edits for datasci cluster

--- src/main/scala/ca/uwaterloo/cs451/a5/Q1.scala
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q1.scala
@@ -71,7 +71,8 @@ object Q1 {
     println("ANSWER=" + count.value)
 
     //TODO:REMOVE
-    val parquet = "TPC-H-0.1-PARQUET"
+    //    val parquet = "TPC-H-0.1-PARQUET"
+    val parquet = "/data/cs451/TPC-H-10-PARQUET"
 
 
     val sqlContext = new SQLContext(sc)
