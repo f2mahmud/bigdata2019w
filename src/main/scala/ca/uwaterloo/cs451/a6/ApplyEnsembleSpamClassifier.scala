8d8d7236c667730358a617a73b37d8500dff7f04
Fahim Mahmud <f2mahmud@uwaterloo.ca>
2019-03-28 01:44:31 -0400

    Finished a6

---
+++ src/main/scala/ca/uwaterloo/cs451/a6/ApplyEnsembleSpamClassifier.scala
@@ -0,0 +1,110 @@
+package ca.uwaterloo.cs451.a6
+
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.log4j.Logger
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import org.rogach.scallop.{ScallopConf, ScallopOption}
+
+class ApplyEnsembleSpamClassifierConf(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(input, model)
+  val input: ScallopOption[String] = opt[String](descr = "input test instances", required = true)
+  val output: ScallopOption[String] = opt[String](descr = "output path", required = true)
+  val model: ScallopOption[String] = opt[String](descr = "classifier model", required = true)
+  val method: ScallopOption[String] = opt[String](descr = "method of merging results", required = true)
+  verify()
+}
+
+object ApplyEnsmbleSpamClassifier {
+
+  def spamminess(w: scala.collection.Map[Int, Double], features: Array[Int]): Double = {
+    var score = 0d
+    features.foreach(f => if (w.contains(f)) score += w(f))
+    score
+  }
+
+  def classify(sc: SparkContext, input: String, model: scala.collection.Map[Int, Double]): RDD[((String, String), (Double, Double))]
+  = {
+    sc.textFile(input)
+      .map(line => {
+        val items = line.split(" ")
+        val features = items.slice(2, items.size - 1).map(_.toInt)
+        val spamValue = spamminess(model, features)
+        ((items(0), items(1)), (spamValue, 1.0))
+      })
+  }
+
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]) {
+
+    val args = new ApplyEnsembleSpamClassifierConf(argv)
+
+    val conf = new SparkConf().setAppName("Spam Classifier")
+    val sc = new SparkContext(conf)
+
+
+    log.info("input : " + args.input())
+    log.info("output : " + args.output())
+    log.info("model : " + args.model())
+    log.info("method : " + args.method())
+
+    val models = FileSystem.get(sc.hadoopConfiguration).listFiles(new Path(args.model()), false)
+
+    var model = sc.broadcast(sc.textFile(models.next().getPath.getName)
+      .map(line => {
+        val items = line.substring(1, line.length - 1).split(",")
+        items(0).toInt -> items(1).toDouble
+      }).collectAsMap())
+
+    val results = classify(sc, args.input(), model.value)
+
+
+    while (models.hasNext) {
+      model = sc.broadcast(sc.textFile(models.next().getPath.getName)
+        .map(line => {
+          val items = line.substring(1, line.length - 1).split(",")
+          items(0).toInt -> items(1).toDouble
+        }).collectAsMap())
+
+      results.union(classify(sc, args.input(), model.value))
+    }
+
+    if (args.method().equals("average")) {
+
+      results.reduceByKey((accum, value) => {
+        (accum._1 + value._1, accum._2 + value._2)
+      }).map(item => {
+        val spamValue: Double = item._2._1 / item._2._2
+        var spamOrHam = "ham"
+        if (spamValue > 0) {
+          spamOrHam = "spam"
+        }
+
+        (item._1, item._2, spamValue, spamOrHam)
+
+      })}else {
+
+        results.map(item => {
+          var spamOrHam = -1    //ham
+          if (item._2._1 > 0) {   //if spam
+            spamOrHam = 1
+          }
+          (item._1,spamOrHam)
+        }).reduceByKey(_+_)
+        .map(item => {
+          if(item._2 > 0){
+            (item._1._1, item._1._2, item._2, "spam" )
+          }else{
+            (item._1._1, item._1._2, item._2, "ham" )
+          }
+        })
+
+      }
+
+    results.saveAsTextFile(args.output())
+
+
+    }
+
+}
--- src/main/scala/ca/uwaterloo/cs451/a6/TrainSpamClassifier.scala
+++ src/main/scala/ca/uwaterloo/cs451/a6/TrainSpamClassifier.scala
@@ -11,6 +11,7 @@ class TrainSpamClassifierConf(args: Seq[String]) extends ScallopConf(args) {
   mainOptions = Seq(input, model)
   val input: ScallopOption[String] = opt[String](descr = "input path", required = true)
   val model: ScallopOption[String] = opt[String](descr = "output path", required = true)
+  val shuffle: ScallopOption[Boolean] = toggle("text", default = Some(false))
   verify()
 }
 
@@ -44,7 +45,7 @@ object TrainSpamClassifier {
     log.info("input : " + args.input)
     log.info("model : " + args.model)
 
-    val fileItems = sc.textFile(args.input())
+    var fileItems = sc.textFile(args.input())
       .map(line => {
         val lineArray = line.split(" ")
         var isSpam: Double = 0.0
@@ -53,10 +54,19 @@ object TrainSpamClassifier {
         }
         val features: Array[Int] = lineArray.slice(2, lineArray.size - 1).map(_.toInt)
         (0, (lineArray(0), isSpam, features))
-      }).groupByKey(1)
+      }).groupByKey(1).map(_._2)
+
+
+    if (args.shuffle.apply()) {
+
+      val r = scala.util.Random
+      fileItems.flatMap(item => {
+        List((r.nextInt(),item.head))
+      }).sortByKey().map(_._2)
+    }
 
     val trainedData = fileItems.foreach(item => {
-      item._2.foreach(subItem => {
+      item.foreach(subItem => {
         val prob = 1.0 / (1.0 + Math.exp(-spamminess(subItem._3)))
         subItem._3.foreach(f => {
           if (w.contains(f)) {
@@ -69,7 +79,6 @@ object TrainSpamClassifier {
     })
 
     val model = sc.parallelize(w.toSeq).repartition(1).saveAsTextFile(args.model())
-
 
 
   }
