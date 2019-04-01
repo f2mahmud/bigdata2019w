1d8b336e8314a269dacf537a3983476ad78d61e4
Fahim Mahmud <f2mahmud@uwaterloo.ca>
2019-04-01 12:53:01 -0400

    testing

--- src/main/scala/ca/uwaterloo/cs451/a7/TrendingArrivals.scala
+++ src/main/scala/ca/uwaterloo/cs451/a7/TrendingArrivals.scala
@@ -106,12 +106,7 @@ object TrendingArrivals {
           }
         })
       .reduceByKeyAndWindow((x: Int, y: Int) => x + y, (x: Int, y: Int) => x - y, Minutes(10), Minutes(10))
-
-      
-      wc.foreachRDD( item => item.foreach(item => println(">>>!! rdd: " + item )))
-
-
-      wc.mapWithState(StateSpec.function(stateUpdateFunction _))
+      .mapWithState(StateSpec.function(stateUpdateFunction _))
       .persist()
 
     wc.saveAsTextFiles(args.output())
